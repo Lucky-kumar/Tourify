549e08e1069f76b73cda1b68eabb8482d83317dd
Lucky Kumar <imt_2020051@iiitm.ac.in>
2022-10-21 22:13:49 +0530

    add geocoder

--- frontend/src/components/Mapbox.js
+++ frontend/src/components/Mapbox.js
@@ -1,5 +1,6 @@
 import Map, { FullscreenControl, GeolocateControl, NavigationControl, Source, Layer } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Geocoder from './Geocoder';
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
@@ -16,6 +17,7 @@ const Mapbox = () => {
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={TOKEN}
     >
+      <Geocoder/>
       <FullscreenControl style={{ marginRight: 40 }} position="bottom-right" />
       <GeolocateControl style={{ marginLeft: 40 }} position="top-left" />
       <NavigationControl style={{ marginLeft: 40 }} position="top-left" />
