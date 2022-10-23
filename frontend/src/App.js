480c356fa3b998750b588b17a6a517a6d0f2fa56
p5111 <sarkarpriyankaa7777@gmail.com>
2022-10-23 19:32:38 +0530

    import sidebar in app,js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
-
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <>
