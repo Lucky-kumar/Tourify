adb6c9ab93226596b88fe8ae6137c92dc6b06259
ektak67 <ektak1002@gmail.com>
2022-11-02 11:00:48 +0530

    chnages

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,8 +1,51 @@
 import React from 'react'
+import "./Register.css"
+import GoogleIcon from '@mui/icons-material/Google';
+// import { building } from './images/building-2817474.svg'
+// import {airlpane} from "./images/airlpane"
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="page">
+      <div id="registration_container">
+       
+        <form action="" >
+        <h1>Tourify</h1>
+          <div className="form-body" id="form-body">
+            <input id="name" placeholder='Name' type="text" required />
+            <span></span>
+            <input id="email" placeholder='Email' type="email" required />
+            <span></span>
+            <input id="password" placeholder='password' type="password" required />
+            <span></span>
+            <input id="confirmpassword" placeholder='Re-password' type="passowrd" required />
+            <span></span>
+            <input id="contact_number" placeholder='Contact Number' type="number" required />
+            <span></span>
+            <input id="address" placeholder='Address' type="text" required />
+            <span></span>
+            <button className='btn'>
+              Register</button>
+            {/* <Link to={}> */}
+
+            {/* </Link> */}
+
+            <h4>Or</h4>
+
+            <div style={{ color: "red", margin: "10px" }}
+              className="google">
+              <div className='title-google' >Login with Google</div>
+              <GoogleIcon></GoogleIcon>
+
+            </div>
+          </div>
+        </form>
+
+        {/* <Link to={'/Login.js'}> */}
+        <div className='change'>Login Now</div>
+        {/* </Link> */}
+      </div>
+    </div>
   )
 }
 
