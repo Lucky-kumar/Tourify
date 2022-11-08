fe546da131b9ad58d6ce082d895e42fe5926c519
ektak67 <ektak1002@gmail.com>
2022-11-08 18:03:51 +0530

    Chnages Register

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import "./Register.css"
+import "./R.css"
 import GoogleIcon from '@mui/icons-material/Google';
-import {image1} from './images/computer-6242945.svg'
-import {aeroplane} from './images/airplane.png'
-import {cloud} from './images/cloud-303182.svg'
-import {PeopleT} from './images/PeopleT.webp'
+import cloud from "../pages/images/cloud-303182.svg";
+import image1 from "../pages/images/computer-6242945.svg"
+import aeroplane from "../pages/images/airplane.png"
+import people from "../pages/images/peoplee.webp"
 
 const Register = () => {
   return (
     <div className="page">
       <div className="images" style={{ display: "flex", flexWrap: "wrap", justifyContent: "space-between" }}>
-      <img src={cloud} alt="" srcset="" className="cloud image" style={{left:"600px", top:"20px"}}/>
-      <img src={cloud} alt="" srcset="" className="cloud image" style={{left:"400px", top:"-50px"}}/>
-      <img src={cloud} alt="" srcset="" className="cloud image" style={{left:"950px"}}/>
-      <img src={cloud} alt="" srcset="" className="cloud image" style={{left:"1350px", width:"100px"}}/>
-      <img src={cloud} alt="" srcset="" className="cloud image" style={{left:"300px", width:"100px",top:"150px"}}/>
-      <img src={cloud} alt="" srcset="" className="cloud image" style={{ top:"-18px" ,width:"200px"}}/>
-        <img src={PeopleT} alt="" srcset="" className="people image" style={{}}/>
-        <img src={image1} alt="" srcset="" className="image_1 image" style={{left:"500px"}}/>
-        <img src={aeroplane} alt="" srcset="" className="plane image" style={{left: "50px"}}/>
+      <img src={cloud} alt="" className="cloud image" style={{left:"600px", top:"20px"}}/>
+      <img src={cloud} alt=""  className="cloud image" style={{left:"400px", top:"-50px"}}/>
+      <img src={cloud} alt=""  className="cloud image" style={{left:"950px"}}/>
+      <img src={cloud} alt=""  className="cloud image" style={{left:"1350px", width:"100px"}}/>
+      <img src={cloud} alt=""  className="cloud image" style={{left:"300px", width:"100px",top:"150px"}}/>
+      <img src={cloud} alt="" className="cloud image" style={{ top:"-18px" ,width:"200px"}}/>
+        <img src={people} alt=""  className="people image" style={{}}/>
+        <img src={image1} alt=""  className="image_1 image" style={{left:"500px"}}/>
+        <img src={aeroplane} alt=""  className="plane image" style={{left: "50px"}}/>
         </div>
       <div id="registration_container">
        
@@ -37,7 +37,7 @@ const Register = () => {
             <span></span>
             <input id="address" placeholder='Address' type="text" required />
             <span></span>
-            <button className='btn'>
+            <button className='bttn'>
               Register</button>
             {/* <Link to={}> */}
 
@@ -45,10 +45,9 @@ const Register = () => {
 
             <h4>Or</h4>
 
-            <div style={{ color: "red", margin: "10px" }}
-              className="google">
-              <div className='title-google' >Login with Google</div>
-              <GoogleIcon className='icon' />
+            <div className="Google">
+              <div className='title-google' >Login with Google
+              <GoogleIcon className='Icon' /></div>
             </div>
           </div>
         </form>
