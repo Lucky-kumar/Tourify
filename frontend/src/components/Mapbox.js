0199b4bdef986f5e81d78b049e0686cab30d4fc4
Lucky Kumar <imt_2020051@iiitm.ac.in>
2022-11-09 01:28:59 +0530

    add pins backend

--- frontend/src/components/Mapbox.js
+++ frontend/src/components/Mapbox.js
@@ -2,25 +2,42 @@ import Map, { FullscreenControl, GeolocateControl, NavigationControl, Source, La
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Geocoder from './Geocoder';
 import RoomIcon from '@mui/icons-material/Room';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import "../App.css"
+import axios from 'axios'
 
 
 const TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 
 const Mapbox = () => {
+  const [pins, setPins] = useState([]);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
-    longitude: 78.182831,
-    latitude: 26.218287,
-    zoom: 10
+    longitude: 78.169894,
+    latitude:  26.249354,
+    zoom: 14
 
   })
+
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const res = await axios.get("/pins");
+        setPins(res.data);
+        console.log(res.data)
+
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins()
+  }, [])
   return (
     <Map
       initialViewState={viewport}
@@ -29,37 +46,41 @@ const Mapbox = () => {
       mapboxAccessToken={TOKEN}
       onViewportChange={nextViewport => setViewport(nextViewport)}
     >
-      <Marker
-        latitude={26.218287}
-        longitude={78.182831}
-        offsetLeft={-20}
-        offsetTop={-10}
-        style={{borderRadius:"8px"  }}
-      >
-        <RoomIcon style={{ fontSize: viewport.zoom * 5 }} />
+      {pins.map(p => (
+        <>
 
-      </Marker>
-      <Popup longitude={78.182831} latitude={26.218287}
-        anchor="bottom"
-        closeButton={true}
-        closeOnClick={false}
-     
-      >
-        <div className="card" style={{backgroundColor:"lightblue", marginTop:"2px" }}>
-          <label style={{color: "red" , fontSize:"18px", marginLeft:"0.4em"}} >Place</label>
-          <h4 className="place" style={{color: "Black" , fontSize:"20px", marginLeft:"-8em", backgroundColor:"transparent"}} >Gwalior</h4>
-          <label style={{color: "red" , fontSize:"18px", marginLeft:"0.4em"}} >Review</label>
-          <p className="desc" style={{fontSize:"20px", marginLeft:"0.3em" ,color:"black" , fontWeight:"bold"}} >Good Place</p>
-          <label  style={{color: "red" , fontSize:"18px", marginLeft:"0.5em" }}>Rating</label>
-          <Rating 
-            name="simple-controlled"
-            value={4}
-            style={{fontWeight:"bold" , marginLeft:"0.5em" }}
-          />
+          <Marker
+            latitude={p.lat}
+            longitude={p.lng}
+            offsetLeft={-20}
+            offsetTop={-10}
+            style={{ borderRadius: "8px" }}
+          >
+            <RoomIcon style={{ fontSize: viewport.zoom * 5 }} />
 
-          <span className="date" style={{marginLeft:"0.5em"}} >created 2 mins ago</span>
-        </div>
-      </Popup>
+          </Marker>
+          <Popup longitude={p.lng} latitude={p.lat}
+            anchor="bottom"
+            closeButton={true}
+            closeOnClick={false}
+          >
+            <div className="card" style={{ marginTop: "2px" }}>
+              <label style={{ color: "red", fontSize: "18px", marginLeft: "0.4em" }} >Place</label>
+              <h4 className="place" style={{ color: "Black", fontSize: "20px", backgroundColor: "transparent" }} >{p.title}</h4>
+              <label style={{ color: "red", fontSize: "18px", marginLeft: "0.4em" }} >Review</label>
+              <p className="desc" style={{ fontSize: "20px", marginLeft: "0.3em", color: "black", fontWeight: "bold" }} >{p.desc}</p>
+              <label style={{ color: "red", fontSize: "18px", marginLeft: "0.5em" }}>Rating</label>
+              <Rating
+                name="simple-controlled"
+                value={p.rating}
+                style={{ fontWeight: "bold", marginLeft: "0.5em" }}
+              />
+
+              <span className="date" style={{ marginLeft: "0.5em" }} >created 2 mins ago</span>
+            </div>
+          </Popup>
+        </>
+      ))}
       <Geocoder />
       <FullscreenControl style={{ marginRight: 40 }} position="bottom-right" />
       <GeolocateControl style={{ marginLeft: 40 }} position="top-left" />
