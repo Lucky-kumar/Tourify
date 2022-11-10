0b75338e6d9bc6027a0e9ae6f3a7b5a05825c0e6
Lucky Kumar <imt_2020051@iiitm.ac.in>
2022-11-10 19:07:16 +0530

    call api

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import Mapbox from './../components/Mapbox';
 import Mapbox from './../components/Mapbox';
 import Navbar from './../components/Navbar'
 import Sidebar from '../components/Sidebar';
 import { Grid } from '@mui/material';
 import { flexbox } from '@mui/system';
+import { getPlacesData } from '../api';
 
 const Home = () => {
+
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    getPlacesData()
+      .then((data) => {
+        console.log(data);
+        setPlaces(data);
+      })
+
+  }, []);
+
+
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/> */}
       <Grid container spacing={1} style={{ width: '100%' }}>
-      <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={4}>
 
-        <div className='sidebar_container'>
-          <Sidebar />
-        </div>
+          <div className='sidebar_container'>
+            <Sidebar />
+          </div>
         </Grid>
         <Grid item xs={12} md={8}>
 
-        <div className='map_container'>
-          <Mapbox />
-        </div>
+          <div className='map_container'>
+            <Mapbox />
+          </div>
         </Grid>
 
       </Grid>
