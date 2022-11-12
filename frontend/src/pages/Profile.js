1432f713814f2b7339ae22880e444e59c251ce97
Dipti1202 <dolldip2577@gmail.com>
2022-11-12 14:08:45 +0530

    change profile css

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -11,32 +11,40 @@ const Profile = () => {
           <div className='content'>
           <div className='info'>
           <InputLabel className='profile_labels'>Name</InputLabel>
+          <div className=''></div>
           </div>
           
           <div className='info'>
           <InputLabel className='profile_labels'>Address</InputLabel>
+          <div></div>
           </div>
           
           <div className='info'>
           <InputLabel className='profile_labels'>Contact Number</InputLabel>
+          <div></div>
           </div>
           
           <div className='info'>
           <InputLabel className='profile_labels'>Email Id</InputLabel>
+          <div></div>
           </div>
           
           <div className='info'>
           <InputLabel className='profile_labels'>Location</InputLabel>
+          <div></div>
           </div>
           
           <div className='info'>
           <InputLabel className='profile_labels'>Address</InputLabel>
+          <div></div>
           </div>
           <hr style={{width:"90%", borderTop:"2px solid gray",borderBottom:"0px"}}/>
           <div className='info'>
           <InputLabel className='profile_labels'>Pins</InputLabel>
+          <div></div>
           </div>
-          <Button className='LogOut'>Log Out</Button>
+          <div>
+          <Button className='LogOut' styles={{marginTop:"20px", marginBottom:"20px"}}>Log Out</Button></div>
           </div>
 
         
