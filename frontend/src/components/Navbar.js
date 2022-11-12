04b9188c21894a0cc4c581fd7b2d6a9eccf55258
Dipti1202 <dolldip2577@gmail.com>
2022-11-12 21:51:36 +0530

    implement logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,24 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./styles/Navbar.css"
+import { AuthContext } from './../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital@1&family=Dancing+Script:wght@600&family=Lobster&display=swap" rel="stylesheet"></link>
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  if (loading) return "Loading...";
+
+  if (error) console.log(error);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGOUT" });
+    try {
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div>
       <nav>
 
         <div className="navbar">
           <ul>
-            <div className="first" style={{fontFamily: 'Lobster'}}>
+            <div className="first" style={{ fontFamily: 'Lobster' }}>
               <li >Tourify </li>
             </div>
             <div className='sec'>
               <li><a href='#'>Home</a></li>
-              <li><a href='#'>About Us</a></li>
-              <li><a href='#'>Contact Us</a></li>
               <li><a href='#'>Profile</a></li>
               <li>
-                <button>Log Out</button>
+                <button onClick={handleLogout}>Log Out</button>
               </li>
             </div>
 
@@ -29,4 +48,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar
